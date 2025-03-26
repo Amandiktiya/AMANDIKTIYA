35008fc249b4f2f093356248ef2aee741f75a5bc
Aman Diltiya <146603281+Amandiktiya@users.noreply.github.com>
2025-03-26 21:36:05 +0530

    practical 1
    practical 1

---
+++ for.cpp
@@ -0,0 +1,11 @@
+// Programming exercises on control flow statements in C++
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 5; i++) {
+        std::cout << "Iteration: " << i << std::endl;
+    }
+
+    return 0;
+}
